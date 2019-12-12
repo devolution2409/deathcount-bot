569b70aa1443568cc824a4a4764f1acd7e340ea1
devolution2409 <contact@davidbouet.fr>
2019-12-12 19:34:52 +0000

    added setter for method

--- src/include/Matcher.hpp
+++ src/include/Matcher.hpp
@@ -5,32 +5,36 @@
 
 
 class Matcher{
-    enum MatcherMethod : int{
-        CV_TM_SQDIFF = 0,
-        CV_TM_SQDIFF_NORMED,
-        CV_TM_CCORR,
-        CV_TM_CCORR_NORMED,
-        CV_TM_CCOEFF,
-        CV_TM_CCOEFF_NORMED
+    enum Method : int{
+        CV_TM_SQDIFF        =0,
+        CV_TM_SQDIFF_NORMED =1,
+        CV_TM_CCORR         =2,
+        CV_TM_CCORR_NORMED  =3,
+        CV_TM_CCOEFF        =4,
+        CV_TM_CCOEFF_NORMED =5
 
 };
     public:
         Matcher();
-        
+
         Matcher& SetTemplate(cv::Mat * other);
         Matcher& SetImage(cv::Mat * other);
         Matcher& SetTemplate(std::string other);
         Matcher& SetImage(std::string other);
         Matcher& SetTemplate(char* other);
         Matcher& SetImage(char* other);
+        Matcher& SetMethod(Matcher::Method method);
 
+        void Process();
 
         ~Matcher();
     private:
     /* https://github.com/opencv/opencv/blob/c2b6c6743175241d895fe4e67028fd960a405b00/modules/core/include/opencv2/core/mat.hpp#L791 */
         cv::Mat * _template;
         cv::Mat * image;
+        cv::Mat * result;
+        Matcher::Method method;
 
-
+    
 
 };
